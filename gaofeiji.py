05cec1d5b865ef7a92ad2a4d9c8031bb08ad5d5c
brainliu <406096277@qq.com>
2018-06-04 16:57:20 +0800

    Merge remote-tracking branches 'remotes/origin/master' and 'remotes/origin/waxx' into brian

--- gaofeiji.py
+++
@@ -1,5 +0,0 @@
-#-*-coding:utf8-*-
-#user:brian
-#created_at:2018/6/1 16:31
-# file: gaofeiji.py
-#location: china chengdu 610000
--- testchange.py
+++
@@ -1,5 +0,0 @@
-#-*-coding:utf8-*-
-#user:brian
-#created_at:2018/6/1 16:07
-# file: testchange.py
-#location: china chengdu 610000
